331becfdcb25a0d1a100001f0c658555dde4f339
Sanatkhot <119149187+Sanatkhot@users.noreply.github.com>
2023-03-27 00:52:27 +0530

    added another print

--- src/GQTCodes/Program1.java
+++ src/GQTCodes/Program1.java
@@ -3,8 +3,9 @@ package GQTCodes;
 public class Program1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	  // TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
 	}
 
 }
